42ad790ff9915dbb2c35a27a817ae070232ac4ad
Andy Shora <Andy.Shora@rga.com>
2014-09-18 15:06:36 +0100

    Browserify bundling moved to gulp task.

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var browserify = require('browserify');
+
+var literalify = require('literalify');
+var source = require('vinyl-source-stream');
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('bundle', function() {
+    var b = browserify();
+    b.transform(literalify.configure({ react: 'window.React' }));
+    b.add('./scripts/my-app.js');
+    b.require('./scripts/my-app.js');
+
+    return b.bundle()
+      .pipe(source('./scripts/my-app.js'))
+      .pipe(rename('_bundle.js'))
+      .pipe(gulp.dest('./scripts'));
+});
+
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('scss/*.scss', ['sass']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
